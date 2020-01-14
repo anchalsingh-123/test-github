d5a11b43d31daeb9500c5014b7d6fdef4a35e66f
anchalsingh-123 <anchal.singh@aptusdatalabs.com>
2020-01-14 14:30:52 +0530

    5th change

--- test_python.py
+++ test_python.py
@@ -1,6 +1,10 @@
 print("Hello world")
+<<<<<<< HEAD
 print("Hi! there")
 print("Git hub Training")
 print("Seesions")
 print("1st session")
 print("Had your lunch?")
+print("See ya!")
+=======
+>>>>>>> parent of 09cca96... 4th change
